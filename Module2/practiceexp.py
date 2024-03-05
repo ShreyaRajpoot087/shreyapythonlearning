83177f19102784c20daba02dcebaff0742c390e8
shreyarajpoot087 <chhoti2301@gmail.com>
2024-03-05 19:51:34 +0530

    Hi , this is the latest code

---
+++ Module2/practiceexp.py
@@ -0,0 +1,30 @@
+average_rating = input('Please provide your avg rating: ')
+print(average_rating * 3)
+
+a = int(input('First number: '))
+b = int(input('Second number: '))
+
+a = a % b
+b = b % a
+
+print(a, b)
+
+print(1 // 2 * 3)
+print(1 / 1)
+
+number = int(input('Please provide a number: '))
+print(number)
+
+#first_name = 'John'
+#print(sep=' - ', 'Your first name is', first_name, 'Welcome!')
+x = 0
+y = 1
+a = x ^ y
+b = x | y
+c = x & y
+print(a + b + c)
+
+
+
+
+
---
+++ Module2/typecasting.py
@@ -0,0 +1,23 @@
+height_cm =input("Enter your height in cm: ")
+float_height_cm = float(height_cm)
+print('Your height is in fit',float_height_cm)
+# second way of declaration
+height_cm =float(input("Enter your height in cm: "))
+print('Your height is in fit',height_cm)
+#
+year_born =int(input("What year were you born? "))
+print('In 2100,you will be',2100-year_born ,'years provided you live this long!')
+
+#calculate temprature
+temp_c=input('Enter temperature in Celsius: ')
+temp_f=float(temp_c) *1.8+32
+temp_statement =str_temp(temp_c) + 'degrees Celcius equals' + str_temp(temp_f) + 'degrees Fahrenheit'
+print(temp_statement)
+
+len('hello')
+# bitwise operator in python there are 5 bitwise operator
+# & ,|,^,logical not ~  there are also shift operator left shift and right shift
+#12<<1 - twice of that number= 24
+#12>>1 - original value divided by 2 = 6
+
+
