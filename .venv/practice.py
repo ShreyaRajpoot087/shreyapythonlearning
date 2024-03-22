903a30dbc404c90c101ea0408cb96f7442e23b05
shreyarajpoot087 <chhoti2301@gmail.com>
2024-03-22 15:10:37 +0530

    this is my python

---
+++ .venv/practice.py
@@ -0,0 +1,48 @@
+x = -10
+if x < 0:
+    print("The negative number ",  x, " is not valid here.")
+print("This is always printed")
+
+if 4 + 5 == 10:
+    print("TRUE")
+else:
+    print("FALSE")
+print("TRUE")
+
+a = 5
+b = 10
+if b < a:
+  print("a is greater than b")
+elif a == b:
+  b = 5
+  print("a and b are equal")
+else:
+  print("b is greater than a")
+
+  x = 0
+  a = 6
+  b = 6
+  if a > 0:
+      if b < 0:
+          x = x + 6
+      elif a > 6:
+          x = x + 5
+      else:
+          x = x + 4
+  else:
+      x = x + 3
+
+  print(x)
+
+  x = 6
+  print(x > 4 and x < 12)
+
+  is_hungry = True
+  if (not is_hungry):
+      print("You are not hungry")
+  else:
+      print("You are hungry")
+
+      x = 6
+      y = 7
+      print(x == y)
---
+++ print.py
@@ -0,0 +1,44 @@
+print('hello shreya welcome to Python')
+
+print(1, 2, 3, 4, sep='#', end='&')
+
+print("Hello" + " " "future" + " " "python!")
+
+
+print(10 - 6 ** 2 / 9 * 10 + 1)
+
+print(2 ** 3.)
+
+print(13 / 4 + 13 % 4)
+print(2 * 3 ** 3 * 4)
+
+x = 10 / 4
+y = 5 / 2.0
+print (x + y)
+
+age = 22
+AGE = 44
+
+age /= 2
+
+print(age + AGE)
+
+amount = 4
+cost = 2
+cost += 2
+print(amount * cost)
+
+name = "Sally"  # employee name
+
+data = "#123"
+print(name + data)
+
+inputString = input('Enter a string: ')
+print(inputString, sep='#', end='&')
+
+age =input("My age is: " )
+print (age)
+
+
+print(int(15.5)-10)
+
